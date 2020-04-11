a45f3973dedae982d518033c72dfb75f1b2a4ad3
roshanthashrestha <roshanthas239@gmail.com>
2020-04-11 16:54:43 -0500

    addition of for loop #1289

--- demo/src/com/roshan/Main.java
+++ demo/src/com/roshan/Main.java
@@ -3,6 +3,13 @@ package com.roshan;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hi, How are you?");
+
+
+        System.out.println("Hi, How are you?");
+
+                for (int i=0;i<=10;i++){
+                    System.out.println(i);
+        }
+
     }
 }
